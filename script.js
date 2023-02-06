118c50aa8b2f8ee50ac8b0336a7aea0ad81e7351
gokulraj00 <107303249+gokulraj00@users.noreply.github.com>
2023-02-06 19:20:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor(v){
+    document.getElementsByTagName('body')[0].style.backgroundColor = v.getAttribute('value');
+}
+var input=document.getElementsByTagName('input')[0];
+input.oninput=function(){
+    document.getElementsByTagName('body')[0].style.backgroundColor =input.value;
+}
